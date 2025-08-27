d3398795080bf081c529a22fb8b05fc71ecd5a8e
juanbisaguirre <96091064+juanbisaguirre@users.noreply.github.com>
2025-08-26 23:16:33 -0300

    Add files via upload

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -5,7 +5,7 @@ products.forEach((product) => {
   const productCard = document.createElement("div");
 
   productCard.innerHTML = `
-    <img src="${product.img}" />
+    <img src="${product.image}" />
     <h2>${product.productName}</h2>
     <p>Precio: $${product.price}</p>
     <p>Cantidad: ${product.quantity} U.</p>
@@ -32,7 +32,7 @@ products.forEach((product) => {
         id: product.id,
         productName: product.productName,
         price: product.price,
-        img: product.img,
+        image: product.image,
         quantity: product.quantity,
       });
     console.log(cart);
--- JavaScript/js/products.js
+++ JavaScript/js/products.js
@@ -4,28 +4,27 @@ const products = [
         productName: "Banana",
         price: 480,
         quanty: 1,
-        image: "/01-Ecommerce2024/media/banana.png",
+        image: "media/banana.png",
     },
-   {
+    {
         id: 2,
         productName: "Leche",
         price: 950,
         quanty: 1,
-        image: "/01-Ecommerce2024/media/leche.png",
+        image: "media/leche.png",
     },
     {
         id: 3,
         productName: "Pollo",
         price: 750,
         quanty: 1,
-        image: "/01-Ecommerce2024/media/pollo.png", 
+        image: "media/pollo.png",
     },
     {
         id: 4,
         productName: "Mayonesa",
         price: 510,
         quanty: 1,
-        image: "/01-Ecommerce2024/media/mayonesa.png",
+        image: "media/mayonesa.png",
     },
-
-] 
+];
