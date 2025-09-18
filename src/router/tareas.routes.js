9f84be33eaea37428a6c4e796ef51499e8ea05f9
juan.bisaguirre <ja.bisaguirrelivellara@gmail.com>
2025-09-18 13:51:06 -0300

    Subiendo proyecto inicial

---
+++ src/router/tareas.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { actualizarTarea, crearTarea, eliminarTarea, listarTarea, listarTareas } from "../controllers/tareas.controller";
+
+
+const router = Router();
+
+router.get("/", listarTareas);
+
+router.get("/:id", listarTarea);
+
+router.post("/", crearTarea);
+
+router.put("/:id", actualizarTarea);
+
+router.delete("/:id", eliminarTarea);
+
+export default router;
